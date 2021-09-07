9b7d85d4559567966b987daecc0432e41cd0e2ae
Ayham Boucher <ayhamboucher@gmail.com>
2021-09-06 21:44:57 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,6 +2,10 @@ from flask import Flask, request, jsonify
 import json
 import logging
 import uuid
+import pandas as pd
+from covidApiClient import CovidApiClient
+from datetime import datetime
+
 
 app = Flask(__name__)
 
@@ -15,4 +19,40 @@ logging.basicConfig(filename=app.config['LOG_FILE'], level=logging.DEBUG)
 @app.route('/')
 def get_covid_reports():
 
-    return jsonify({"Output_file": "{}{}.csv".format(app.config['OUTPUT_FOLDER'], uuid.uuid1())})
+    # Read countries csv
+    try:
+
+        df = pd.read_csv(app.config["INPUT_FILE_NAME"])
+
+    except Exception as e:
+
+        app.logger.error(
+            "couldn't read iso/date (countries.csv) file with error: {}".format(e))
+
+        return jsonify({"error": "couldn't read the requested csv file. Make sure you have an entry in src/config.py with the key: INPUT_FILE_NAME set"})
+
+    # Create an instance of the ApiClient
+    _covidApiClient = CovidApiClient(countries_df=df)
+
+    try:
+
+        results_df = _covidApiClient.get_results()
+
+        # Save dataframe into csv
+        result_uuid = uuid.uuid1()
+
+        results_df.to_csv("{}{}.csv".format(
+            app.config["OUTPUT_FOLDER"], result_uuid))
+
+    except Exception as e:
+
+        app.logger.error(
+            "couldn't retreive the results from the covid-api with error message: {}".format(e))
+
+        return jsonify({"error": "couldn't retreive the results from https://covid-api.com/api/reports/, check the log file in {} for details".format(app.config["LOG_FILE"])})
+
+    if results_df is None:
+
+        return jsonify({"error": "couldn't retreive the results from https://covid-api.com/api/reports/, check the log file in {} for details".format(app.config["LOG_FILE"])})
+
+    return jsonify({"output_file": "{}{}.csv".format(app.config['OUTPUT_FOLDER'], result_uuid)})
